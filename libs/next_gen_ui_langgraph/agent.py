96cace4344434461ecb2624c678c734dd065d37d
Libor Krzyzanek <libor.krzyzanek@gmail.com>
2025-03-12 17:22:24 +0100

    langgraph agent #NGUI-30

--- libs/next_gen_ui_langgraph/agent.py
+++ libs/next_gen_ui_langgraph/agent.py
@@ -1,7 +1,11 @@
-from langgraph.graph import MessagesState, StateGraph
-from next_gen_ui_agent import NextGenUIAgent
-
-workflow = StateGraph(MessagesState)
+from langgraph.graph import StateGraph, START, END
+from langchain_core.runnables.config import RunnableConfig
+from langchain_core.language_models import BaseChatModel
+from next_gen_ui_agent import NextGenUIAgent, AgentInput, InputData
+from next_gen_ui_agent.model import LangChainModelInference
+from .graph_config import AgentState, AgentConfig, AgentInputState, AgentOutputState
+import httpx
+import os
 
 ngui_agent = NextGenUIAgent()
 
@@ -9,12 +13,81 @@ ngui_agent = NextGenUIAgent()
 class NextGenUILangGraphAgent:
     """Next Gen UI Agent in LangGraph."""
 
-    def say_hello(self) -> str:
-        print("Hallo from LangGraph")
+    def __init__(self, model: BaseChatModel):
+        super().__init__()
+        self.inference = LangChainModelInference(model)
 
-        return "langgraph"
+    ### Nodes
+    async def data_selection(
+        self, state: AgentInputState, config: RunnableConfig
+    ) -> AgentState:
+        print("\n\n---CALL data_selection---")
+        backend_data = state.get("backend_data", [])
+        user_prompt = state.get("user_prompt", "")
 
+        if user_prompt and len(backend_data) > 0:
+            print("User_prompt and backend_data taken from state directly")
+            return
 
-if __name__ == "__main__":
-    agent = NextGenUILangGraphAgent()
-    agent.say_hello()
+        messages = state["messages"]
+        # print(messages)
+
+        messagesReversed = list(reversed(messages))
+        for m in messagesReversed:
+            # print(m.content)
+            # TODO: Handle better success/error messages
+            if (
+                m.type == "tool"
+                and m.status == "success"
+                and not m.name.startswith("genie")
+            ):
+                backend_data.append({"id": m.tool_call_id, "data": m.content})
+            if m.type == "human" and not user_prompt:
+                user_prompt = m.content
+            if user_prompt != "" and len(backend_data) > 0:
+                break
+
+        print(
+            f"User_prompt and backend_data taken HumanMessage and ToolMessages. count={len(backend_data)}"
+        )
+        return {
+            "backend_data": backend_data,
+            "user_prompt": user_prompt,
+        }
+
+    async def component_selection(
+        self, state: AgentState, config: RunnableConfig
+    ) -> AgentState:
+        print("\n\n---CALL component_selection---")
+
+        user_prompt = state["user_prompt"]
+        input_data = [InputData(id=d["id"], data=d["data"]) for d in state["backend_data"]]
+        input = AgentInput(user_prompt=user_prompt, input_data=input_data)
+        components = await ngui_agent.component_selection(
+            inference=self.inference,
+            input=input,
+        )
+        return {"components": components}
+
+    def build_graph(self):
+        ### Graph
+        builder = StateGraph(
+            state_schema=AgentState,
+            config_schema=AgentConfig,
+            input=AgentInputState,
+            output=AgentOutputState,
+        )
+
+        builder.add_node("component_selection", self.component_selection)
+        builder.add_node("data_selection", self.data_selection)
+        # builder.add_node("component_system", choose_system)
+        # builder.add_node("design_system_handler", design_system_handler)
+
+        builder.add_edge(START, "data_selection")
+        builder.add_edge("data_selection", "component_selection")
+        builder.add_edge("component_selection", END)
+        # builder.add_edge("component_selection", "component_system")
+        # builder.add_edge("design_system_handler", END)
+
+        graph = builder.compile()
+        return graph
--- libs/next_gen_ui_langgraph/agent_test.py
+++ libs/next_gen_ui_langgraph/agent_test.py
@@ -1,8 +1,108 @@
-# import pytest
+import pytest
+import asyncio
 
+from langchain_core.messages import HumanMessage, ToolMessage
+from langchain_core.language_models import FakeMessagesListChatModel
+
+from next_gen_ui_agent.model import LangChainModelInference
+from next_gen_ui_agent import UIComponentMetadata
 from . import NextGenUILangGraphAgent
 
 
-def test_say_hello() -> None:
-    agent = NextGenUILangGraphAgent()
-    assert agent.say_hello() == "langgraph"
+
+@pytest.mark.asyncio
+async def test_agent() -> None:
+    user_input = "Tell me brief details of Toy Story"
+    movies_data = """
+    [
+    {
+        "movie":{
+        "languages":[
+            "English"
+        ],
+        "year":1995,
+        "imdbId":"0114709",
+        "runtime":81,
+        "imdbRating":8.3,
+        "movieId":"1",
+        "countries":[
+            "USA"
+        ],
+        "imdbVotes":591836,
+        "title":"Toy Story",
+        "url":"https://themoviedb.org/movie/862",
+        "revenue":373554033,
+        "tmdbId":"862",
+        "plot":"A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room.",
+        "posterUrl":"https://image.tmdb.org/t/p/w440_and_h660_face/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg",
+        "released":"1995-11-22",
+        "trailerUrl": "https://www.youtube.com/watch?v=v-PjgYDrg70",
+        "budget":30000000
+        },
+        "actors":[
+        "Jim Varney",
+        "Tim Allen",
+        "Tom Hanks",
+        "Don Rickles"
+        ]
+    }
+    ]
+    """
+    response = """
+    {
+        "title": "Toy Story Details",
+        "reasonForTheComponentSelection": "One item available in the data",
+        "confidenceScore": "100%",
+        "component": "one-card",
+        "fields" : [
+            {"name":"Title","data_path":"movie.title"},
+            {"name":"Year","data_path":"movie.year"},
+            {"name":"IMDB Rating","data_path":"movie.imdbRating"},
+            {"name":"Release Date","data_path":"movie.released"}
+        ]
+    }
+    """
+    # llm_settings = {
+    #     "model": "llama3.2",
+    #     "base_url": "http://localhost:11434/v1",
+    #     "api_key": "ollama",
+    #     "temperature": 0,
+    # }
+    # llm = ChatOpenAI(**llm_settings, disable_streaming=True)
+    msg = {"type": "assistant", "content": response}
+    llm = FakeMessagesListChatModel(responses=[msg])
+
+    agent = NextGenUILangGraphAgent(model=llm)
+    graph = agent.build_graph()
+
+    tool_message = ToolMessage(
+        content=movies_data,
+        name="movies",
+        tool_call_id="call_Jja7J89XsjrOLA5r!MEOW!SL",
+    )
+    # tm_actor1 = ToolMessage(content=actor1, name="actor_detail", tool_call_id="call_5")
+    # tm_actor2 = ToolMessage(content=actor2, name="actor_detail", tool_call_id="call_6")
+
+    human_message = HumanMessage(content=user_input)
+    # async for msg, metadata in graph.astream(
+    #     {"messages": [human_message, tool_message]},
+    #     # {"messages": [human_message, tm_actor1, tm_actor2]},
+    #     # {"configurable": configurable},
+    #     stream_mode="messages",
+    # ):
+    #     print("MMMMSG" + msg.content, end="", flush=True)
+    result = await graph.ainvoke(
+        input={"messages": [human_message, tool_message]},
+        # config={"configurable": configurable},
+    )
+    components = result["components"]
+    c: UIComponentMetadata = components[0]
+    result["messages"] = ["removed"]
+    print("\n--RESULT---")
+    print(c.component)
+    assert "one-card" == c.component
+
+
+
+if __name__ == "__main__":
+    asyncio.run(test_agent())
