c9c59e954dacea6125f986d9ba5c308cdb0cdda1
Libor Krzyzanek <2361054+lkrzyzanek@users.noreply.github.com>
2025-03-12 15:11:05 +0100

    test dependcy management, pytest-asyncio support, python 3.11 #NGUI-40 (#39)

--- libs/next_gen_ui_agent/agent.py
+++ libs/next_gen_ui_agent/agent.py
@@ -1,6 +1,9 @@
 class NextGenUIAgent:
     """Next Gen UI Agent."""
 
+    async def say_hello_async(self) -> str:
+        return "next_gen_ui_agent_async"
+
     def say_hello(self) -> str:
         print("Hallo from main next_gen_ui_agent.NextGenUIAgent")
         return "next_gen_ui_agent"
--- libs/next_gen_ui_agent/agent_test.py
+++ libs/next_gen_ui_agent/agent_test.py
@@ -1,4 +1,4 @@
-# import pytest
+import pytest
 
 from . import NextGenUIAgent
 
@@ -6,3 +6,9 @@ from . import NextGenUIAgent
 def test_say_hello() -> None:
     agent = NextGenUIAgent()
     assert agent.say_hello() == "next_gen_ui_agent"
+
+
+@pytest.mark.asyncio
+async def test_async_say_hello() -> None:
+    agent = NextGenUIAgent()
+    assert await agent.say_hello_async() == "next_gen_ui_agent_async"
