bfcf6f8d38b59ee8cc8f4ac8e8304bf1f3198fc7
Libor Krzyzanek <2361054+lkrzyzanek@users.noreply.github.com>
2025-03-25 15:15:00 +0100

    distribution tests, renderers deps improvements #NGUI-49 (#50)

---
+++ tests/distribution-all/README.md
@@ -0,0 +1,34 @@
+# Tests of all distribution packages
+
+It's expected that previous jobs run package goal:
+
+```sh
+pants package ::
+```
+
+## Run Test
+
+```sh
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+Install depenendcies:
+
+```sh
+sh install_deps.sh
+source .venv/bin/activate
+```
+
+```sh
+pytest .
+```
+
+## Debugging
+
+Dependencies:
+
+```sh
+pip install deptree
+deptree
+```
---
+++ tests/distribution-all/next_gen_ui_agent_test.py
@@ -0,0 +1,49 @@
+import json
+import logging
+
+from next_gen_ui_agent import NextGenUIAgent, UIComponentMetadata
+
+test_component: UIComponentMetadata = {
+    "title": "Toy Story",
+    "reasonForTheComponentSelection": "One item available in the data",
+    "confidenceScore": "100%",
+    "component": "one-card",
+    "fields": [
+        {"name": "Title", "data_path": "movie.title", "data": "Toy Story"},
+        {"name": "Year", "data_path": "movie.year", "data": "1900"},
+        {"name": "IMDB Rating", "data_path": "movie.imdbRating", "data": "222"},
+    ],
+    "id": "test-id",
+}
+
+
+def test_design_system_handler_default_json() -> None:
+    agent = NextGenUIAgent()
+    agent.design_system_handler([test_component])
+    json_str = test_component["rendition"]
+    result: UIComponentMetadata = json.loads(json_str)
+    assert result["title"] == "Toy Story"
+    assert result["fields"][0]["data"] == "Toy Story"
+    assert result["fields"][1]["data"] == "1900"
+
+
+def test_design_system_handler_rhds() -> None:
+    agent = NextGenUIAgent()
+    agent.design_system_handler([test_component], "rhds")
+    rendition = test_component["rendition"]
+    assert "<rh-card" in rendition
+
+
+def test_design_system_handler_patternfly() -> None:
+    agent = NextGenUIAgent()
+    agent.design_system_handler([test_component], "patternfly")
+    rendition = test_component["rendition"]
+    assert "<Card>" in rendition
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    test_design_system_handler_default_json()
+    test_design_system_handler_rhds()
+    test_design_system_handler_patternfly()
