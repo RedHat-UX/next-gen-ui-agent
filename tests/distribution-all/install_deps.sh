00fa6e61ffb73892f41e527ae1d46683cbef4260
Libor Krzyzanek <libor.krzyzanek@gmail.com>
2025-04-02 09:57:50 +0200

    feat(NGUI-68): publishing next_gen_ui_testing package

--- tests/distribution-all/install_deps.sh
+++ tests/distribution-all/install_deps.sh
@@ -1,16 +1,13 @@
 pip install pytest
 pip install pytest-asyncio
 
-echo Uninstalling all existing packages
-pip uninstall -y ../../dist/next_gen_ui_agent-0.0.1-py3-none-any.whl
-pip uninstall -y ../../dist/next_gen_ui_langgraph-0.0.1-py3-none-any.whl
-pip uninstall -y ../../dist/next_gen_ui_llama_stack-0.0.1-py3-none-any.whl
-pip uninstall -y ../../dist/next_gen_ui_rhds_renderer-0.0.1-py3-none-any.whl
-pip uninstall -y ../../dist/next_gen_ui_patternfly_renderer-0.0.1-py3-none-any.whl
+echo UNINSTALLING all existing packages
+pip uninstall -y next_gen_ui_agent
+pip uninstall -y next_gen_ui_langgraph
+pip uninstall -y next_gen_ui_llama_stack
+pip uninstall -y next_gen_ui_rhds_renderer
+pip uninstall -y next_gen_ui_patternfly_renderer
+pip uninstall -y next_gen_ui_testing
 
-echo Installing all packages
-pip install ../../dist/next_gen_ui_agent-0.0.1-py3-none-any.whl
-pip install ../../dist/next_gen_ui_langgraph-0.0.1-py3-none-any.whl
-pip install ../../dist/next_gen_ui_llama_stack-0.0.1-py3-none-any.whl
-pip install ../../dist/next_gen_ui_rhds_renderer-0.0.1-py3-none-any.whl
-pip install ../../dist/next_gen_ui_patternfly_renderer-0.0.1-py3-none-any.whl
+echo INSTALLING all packages
+pip install ../../dist/next_gen_ui_*.whl
--- tests/distribution-all/next_gen_ui_agent_test.py
+++ tests/distribution-all/next_gen_ui_agent_test.py
@@ -2,19 +2,9 @@ import json
 import logging
 
 from next_gen_ui_agent import NextGenUIAgent, UIComponentMetadata
+from next_gen_ui_testing import data_after_transformation
 
-test_component: UIComponentMetadata = {
-    "title": "Toy Story",
-    "reasonForTheComponentSelection": "One item available in the data",
-    "confidenceScore": "100%",
-    "component": "one-card",
-    "fields": [
-        {"name": "Title", "data_path": "movie.title", "data": "Toy Story"},
-        {"name": "Year", "data_path": "movie.year", "data": "1900"},
-        {"name": "IMDB Rating", "data_path": "movie.imdbRating", "data": "222"},
-    ],
-    "id": "test-id",
-}
+test_component: UIComponentMetadata = data_after_transformation.get_transformed_component()
 
 
 def test_design_system_handler_default_json() -> None:
@@ -22,9 +12,9 @@ def test_design_system_handler_default_json() -> None:
     agent.design_system_handler([test_component])
     json_str = test_component["rendition"]
     result: UIComponentMetadata = json.loads(json_str)
-    assert result["title"] == "Toy Story"
-    assert result["fields"][0]["data"] == "Toy Story"
-    assert result["fields"][1]["data"] == "1900"
+    assert result["title"] == "Toy Story Details"
+    assert result["fields"][0]["data"] == ["Toy Story"]
+    assert result["fields"][1]["data"] == ["1995"]
 
 
 def test_design_system_handler_rhds() -> None:
