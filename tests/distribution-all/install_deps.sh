1f0c5e98117e2079628df43b2e960cf51a8a4fd3
Libor Krzyzanek <libor.krzyzanek@gmail.com>
2025-04-11 12:33:14 +0200

    distribution all installation tuning

--- tests/distribution-all/install_deps.sh
+++ tests/distribution-all/install_deps.sh
@@ -1,13 +1,5 @@
-pip install pytest
-pip install pytest-asyncio
-
 echo UNINSTALLING all existing packages
-pip uninstall -y next_gen_ui_agent
-pip uninstall -y next_gen_ui_langgraph
-pip uninstall -y next_gen_ui_llama_stack
-pip uninstall -y next_gen_ui_rhds_renderer
-pip uninstall -y next_gen_ui_patternfly_renderer
-pip uninstall -y next_gen_ui_testing
+pip uninstall -y ../../dist/next_gen_ui_*.whl
 
 echo INSTALLING all packages
 pip install ../../dist/next_gen_ui_*.whl
