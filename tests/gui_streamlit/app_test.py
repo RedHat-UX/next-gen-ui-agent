7d4d3ab682d8cbea003dad0d21dd58bd004ca019
Libor Krzyzanek <libor.krzyzanek@gmail.com>
2025-04-24 15:23:40 +0200

    feat: Streamlit test GUI supports editing mocked data

--- tests/gui_streamlit/app_test.py
+++ tests/gui_streamlit/app_test.py
@@ -3,15 +3,17 @@ from streamlit.testing.v1 import AppTest
 
 def test_renderers_options():
     at = AppTest.from_file("app.py").run()
-    assert at.selectbox[0].options == ["json", "rhds", "patternfly"]
+    assert at.selectbox("renderer_select").options == ["rhds", "json", "patternfly"]
 
 
 def test_rendering_json():
     """A user increments the number input, then clicks Add"""
-    at = AppTest.from_file("app.py").run()
+    at = AppTest.from_file("app.py")
+    at.session_state.renderer = "json"
+    at.run()
     assert at.text[1].value == "Rendering DONE: json"
     # test that author value is in the agent output
-    assert "Tom Hanks" in at.code[2].value
+    assert "Tom Hanks" in at.code[1].value
 
 
 def test_rendering_rhds():
