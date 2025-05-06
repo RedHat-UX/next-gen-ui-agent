b807eb315aa63063ffa95431ee787578c2a82e2a
Libor Krzyzanek <libor.krzyzanek@gmail.com>
2025-05-06 10:38:54 +0200

    feat(NGUI-133): JSON Schema for one-card component

---
+++ spec/component_schema_test.py
@@ -0,0 +1,64 @@
+import json
+import logging
+import os
+from typing import Any
+
+from next_gen_ui_agent.renderer.one_card import OneCardRenderStrategy
+from next_gen_ui_agent.renderer.types import (
+    CustomGenerateJsonSchema,
+    RenderContextOneCard,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def test_schema_no_title() -> None:
+    schema = RenderContextOneCard.model_json_schema(
+        schema_generator=CustomGenerateJsonSchema
+    )
+    schema_str = json.dumps(schema, indent=2)
+    assert '"title": "Title"' not in schema_str
+
+
+def test_one_card_schema() -> None:
+    schema = RenderContextOneCard.model_json_schema(
+        schema_generator=CustomGenerateJsonSchema
+    )
+    with open(schema_file_path(OneCardRenderStrategy.COMPONENT_NAME), "r") as file:
+        file_content = file.read()
+    assert '"title": "Title"' not in file_content
+    assert json.dumps(schema, indent=2) == file_content
+
+
+def schema_file_path(component_name) -> str:
+    return os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            "./component",
+            f"{component_name}.schema.json",
+        )
+    )
+
+
+def save_schema(component_name: str, schema: dict[str, Any]) -> None:
+    file_path = schema_file_path(component_name)
+    with open(file_path, "w") as file:
+        logger.info("Saving schema for component '%s' to %s", component_name, file_path)
+        file.write(json.dumps(schema, indent=2))
+
+
+def regenerate_schemas() -> None:
+    """Regnerate schema store in /spec/component directory"""
+
+    save_schema(
+        OneCardRenderStrategy.COMPONENT_NAME,
+        RenderContextOneCard.model_json_schema(
+            schema_generator=CustomGenerateJsonSchema
+        ),
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    logger.setLevel(logging.INFO)
+    regenerate_schemas()
