39d910cc3aaaf2186f8757f5596351814b788ec6
Vlastimil Elias <velias@redhat.com>
2025-05-12 16:09:00 +0200

    feat(NGUI-105): llama stack integration changed, now uses inference API

--- libs/next_gen_ui_llama_stack/llama_stack_inference.py
+++ libs/next_gen_ui_llama_stack/llama_stack_inference.py
@@ -1,17 +1,44 @@
 import logging
-from typing import AsyncIterator, Iterator
 
 from llama_stack_client import AsyncLlamaStackClient, LlamaStackClient
-from llama_stack_client.lib.agents.agent import Agent, AsyncAgent
-from llama_stack_client.types.agents.turn import Turn
-from llama_stack_client.types.shared import UserMessage
+from llama_stack_client.types.shared import SystemMessage, UserMessage
+from llama_stack_client.types.shared.chat_completion_response import (
+    ChatCompletionResponse,
+)
+from llama_stack_client.types.shared.sampling_params import (
+    SamplingParams,
+    StrategyGreedySamplingStrategy,
+)
 from next_gen_ui_agent.model import InferenceBase
 
 logger = logging.getLogger(__name__)
 
+# greedy sampling always select the most probable next word, it should also be the fastest sampling method
+LLM_SAMPLING_STRATEGY = StrategyGreedySamplingStrategy(type="greedy")
+
+
+def process_response(response, input_messages) -> str:
+    """Helper method to process response of the client.inference.chat_completion in both agents - validate response type, log inputs and output, return content string"""
+
+    if isinstance(response, ChatCompletionResponse):
+        result = response.completion_message.content
+
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug("UI Agent LLM Inputs: %s", input_messages)
+            logger.debug("UI Agent LLM Output: %s", result)
+
+        if isinstance(result, str):
+            return result
+        return str(result)
+    else:
+        # Should not happen because of `stream=False`, but just to be sure
+        raise NotImplementedError(
+            "Set stream=False on client.inference.chat_completion!"
+        )
+
 
 class LlamaStackAgentInference(InferenceBase):
-    """Class wrapping llama_stack_client.lib.agents.agent.Agent."""
+    """Class wrapping llama_stack LlamaStackClient.inference"""
 
     def __init__(self, client: LlamaStackClient, model: str):
         super().__init__()
@@ -19,36 +46,23 @@ class LlamaStackAgentInference(InferenceBase):
         self.client = client
 
     async def call_model(self, system_msg: str, prompt: str) -> str:
-        agent = Agent(
-            client=self.client,
-            model=self.model,
-            instructions=system_msg,
-            enable_session_persistence=False,
-        )
-        # Create a session
-        session_id = agent.create_session(session_name="ngui")
-        # Non-streaming API
-        response = agent.create_turn(
-            session_id=session_id,
-            messages=[UserMessage(content=prompt, role="user")],
-            stream=False,
-        )
+        input_messages = [
+            SystemMessage(role="system", content=system_msg),
+            UserMessage(role="user", content=prompt),
+        ]
 
-        if isinstance(response, Turn):
-            logger.debug("Inputs: %s", response.input_messages)
-            logger.debug("Output: %s", response.output_message.content)
-            logger.debug("Steps: %s", response.steps)
-            result = response.output_message.content
-            if isinstance(result, str):
-                return result
-            return str(result)
-        if isinstance(response, Iterator):
-            # Should not happen because of `stream=False`
-            raise NotImplementedError("stream=False set on agent.create_turn")
+        response = self.client.inference.chat_completion(
+            model_id=self.model,
+            messages=input_messages,
+            stream=False,
+            sampling_params=SamplingParams(strategy=LLM_SAMPLING_STRATEGY),
+        )  # type: ignore
+
+        return process_response(response, input_messages)
 
 
 class LlamaStackAsyncAgentInference(InferenceBase):
-    """Class wrapping llama_stack_client.lib.agents.agent.AsyncAgent."""
+    """Class wrapping llama_stack AsyncLlamaStackClient.inference"""
 
     def __init__(self, client: AsyncLlamaStackClient, model: str):
         super().__init__()
@@ -56,29 +70,16 @@ class LlamaStackAsyncAgentInference(InferenceBase):
         self.client = client
 
     async def call_model(self, system_msg: str, prompt: str) -> str:
-        agent = AsyncAgent(
-            client=self.client,  # type: ignore
-            model=self.model,
-            instructions=system_msg,
-            enable_session_persistence=False,
-        )
-        # Create a session
-        session_id = await agent.create_session(session_name="ngui")
-        # Non-streaming API
-        response = await agent.create_turn(
-            session_id=session_id,
-            messages=[UserMessage(content=prompt, role="user")],
-            stream=False,
-        )
+        input_messages = [
+            SystemMessage(role="system", content=system_msg),
+            UserMessage(role="user", content=prompt),
+        ]
 
-        if isinstance(response, Turn):
-            logger.debug("Inputs: %s", response.input_messages)
-            logger.debug("Output: %s", response.output_message.content)
-            logger.debug("Steps: %s", response.steps)
-            result = response.output_message.content
-            if isinstance(result, str):
-                return result
-            return str(result)
-        if isinstance(response, AsyncIterator):
-            # Should not happen because of `stream=False`
-            raise NotImplementedError("stream=False set on agent.create_turn")
+        response = await self.client.inference.chat_completion(
+            model_id=self.model,
+            messages=input_messages,
+            stream=False,
+            sampling_params=SamplingParams(strategy=LLM_SAMPLING_STRATEGY),
+        )  # type: ignore
+
+        return process_response(response, input_messages)
