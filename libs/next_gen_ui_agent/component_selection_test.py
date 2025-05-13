22b766b0a58c1856d568565a823ccaa7a494e157
Vlastimil Elias <velias@redhat.com>
2025-05-13 17:33:29 +0200

    feat(NGUI-104): LLM response JSON validation

--- libs/next_gen_ui_agent/component_selection_test.py
+++ libs/next_gen_ui_agent/component_selection_test.py
@@ -3,55 +3,29 @@ import logging
 
 import pytest
 from langchain_core.language_models import FakeMessagesListChatModel
+from next_gen_ui_agent.types import AgentInput
+from pytest import fail
 
 from . import InputData
-from .component_selection import component_selection_run
+from .component_selection import component_selection, component_selection_run
 from .model import LangChainModelInference
 
-# from langchain_openai import ChatOpenAI
-
-
-@pytest.mark.asyncio
-async def test_component_selection_run() -> None:
-    user_input = "Tell me brief details of Toy Story"
-    movies_data = """
+movies_data = """
     [
     {
         "movie":{
-        "languages":[
-            "English"
-        ],
         "year":1995,
-        "imdbId":"0114709",
-        "runtime":81,
         "imdbRating":8.3,
-        "movieId":"1",
         "countries":[
             "USA"
         ],
-        "imdbVotes":591836,
         "title":"Toy Story",
-        "url":"https://themoviedb.org/movie/862",
-        "revenue":373554033,
-        "tmdbId":"862",
-        "plot":"A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room.",
-        "posterUrl":"https://image.tmdb.org/t/p/w440_and_h660_face/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg",
-        "released":"1995-11-22",
-        "trailerUrl": "https://www.youtube.com/watch?v=v-PjgYDrg70",
-        "budget":30000000
-        },
-        "actors":[
-        "Jim Varney",
-        "Tim Allen",
-        "Tom Hanks",
-        "Don Rickles"
-        ]
+        "url":"https://themoviedb.org/movie/862"
     }
     ]
     """
-    input_data = InputData({"id": "1", "data": movies_data})
 
-    response = """
+response = """
     {
         "title": "Toy Story Details",
         "reasonForTheComponentSelection": "One item available in the data",
@@ -65,22 +39,65 @@ async def test_component_selection_run() -> None:
         ]
     }
     """
-    # llm_settings = {
-    #     "model": "llama3.2",
-    #     "base_url": "http://localhost:11434/v1",
-    #     "api_key": "ollama",
-    #     "temperature": 0,
-    # }
-    # llm = ChatOpenAI(**llm_settings, disable_streaming=True)
+
+
+@pytest.mark.asyncio
+async def test_component_selection_run_OK() -> None:
+    user_input = "Tell me brief details of Toy Story"
+    input_data = InputData({"id": "1", "data": movies_data})
+
     msg = {"type": "assistant", "content": response}
     llm = FakeMessagesListChatModel(responses=[msg])  # type: ignore
-    iference = LangChainModelInference(llm)
+    inference = LangChainModelInference(llm)
 
-    result = await component_selection_run(user_input, iference, input_data)
+    result = await component_selection_run(user_input, inference, input_data)
     assert result.component == "one-card"
+
+
+@pytest.mark.asyncio
+async def test_component_selection_run_INVALID_LLM_RESPONSE() -> None:
+    user_input = "Tell me brief details of Toy Story"
+    input_data = InputData({"id": "1", "data": movies_data})
+
+    msg = {"type": "assistant", "content": "invalid json"}
+    llm = FakeMessagesListChatModel(responses=[msg])  # type: ignore
+    inference = LangChainModelInference(llm)
+
+    try:
+        await component_selection_run(user_input, inference, input_data)
+        fail("Exception expected")
+    except Exception as e:
+        assert e.__class__.__name__ == "ValueError"
+        pass
+
+
+@pytest.mark.asyncio
+async def test_component_selection() -> None:
+    input_data_1 = InputData({"id": "1", "data": movies_data})
+    input_data_2 = InputData({"id": "2", "data": movies_data})
+
+    input = AgentInput(
+        {
+            "user_prompt": "Tell me brief details of Toy Story",
+            "input_data": [input_data_1, input_data_2],
+        }
+    )
+
+    msg = {"type": "assistant", "content": response}
+    llm = FakeMessagesListChatModel(responses=[msg])  # type: ignore
+    inference = LangChainModelInference(llm)
+
+    result = await component_selection(inference, input)
+
+    assert len(result) == 2
+    ids = []
+    for r in result:
+        ids.append(r.id)
+    assert "1" in ids
+    assert "2" in ids
 
 
 if __name__ == "__main__":
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
-    asyncio.run(test_component_selection_run())
+    asyncio.run(test_component_selection_run_OK())
