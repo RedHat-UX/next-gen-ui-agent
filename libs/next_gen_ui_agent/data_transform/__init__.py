1e4e5f955d66ea328b0e0aa5027abda06119f619
Vlastimil Elias <velias@redhat.com>
2025-07-09 16:05:07 +0200

    feat(NGUI-227): implemented switching of components available in LLM syste prompt (supported only or all), removed unimplemented chart components

--- libs/next_gen_ui_agent/data_transform/__init__.py
+++ libs/next_gen_ui_agent/data_transform/__init__.py
@@ -1,6 +1,4 @@
 from next_gen_ui_agent.data_transform.audio import AudioPlayerDataTransformer
-from next_gen_ui_agent.data_transform.chart_line import LineChartDataTransformer
-from next_gen_ui_agent.data_transform.chart_pie import PieChartDataTransformer
 from next_gen_ui_agent.data_transform.image import ImageDataTransformer
 from next_gen_ui_agent.data_transform.one_card import OneCardDataTransformer
 from next_gen_ui_agent.data_transform.table import TableDataTransformer
@@ -11,7 +9,5 @@ __all__ = [
     "OneCardDataTransformer",
     "VideoPlayerDataTransformer",
     "AudioPlayerDataTransformer",
-    "LineChartDataTransformer",
-    "PieChartDataTransformer",
     "TableDataTransformer",
 ]
--- libs/next_gen_ui_agent/renderer/chart_line.py
+++
@@ -1,5 +0,0 @@
-from next_gen_ui_agent.renderer.base_renderer import RenderStrategyBase
-
-
-class LineChartRenderStrategy(RenderStrategyBase):
-    COMPONENT_NAME = "chart-line"
