805542a8e80e229bdbb6b0cef44db2f9fdb22561
Vlastimil Elias <velias@redhat.com>
2025-07-10 13:33:25 +0200

    feat(NGUI-227) LLM system prompt tuning for supported components

--- libs/next_gen_ui_agent/component_selection.py
+++ libs/next_gen_ui_agent/component_selection.py
@@ -12,14 +12,14 @@ ui_components_description_all = """
 * table - component to visualize array of data items with size over 6. Better suitable for small number of shown fields with shorter values.
 * set-of-cards - component to visualize array of data items with size up to 6. Better suitable for high numbers of shown fields and for fields withlonger values.
 * one-card - component to visualize one data item.
-* video-player - component to play video from one item data. First field contains title, second url to the video e.g. https://www.youtube.com/watch?v=v-PjgYDrg70
-* image - component to show one image from one item data. First field contains title, second url to the image.
+* video-player - component to play video from one item data. Field must contain url pointing to the video to be shown, e.g. https://www.youtube.com/watch?v=v-PjgYDrg70
+* image - component to show one image from one item data. Field must contain url pointing to the image to be shown, e.g. https://www.images.com/v-PjgYDrg70.jpeg
 """
 
 ui_components_description_supported = """
 * one-card - component to visualize one data item.
-* video-player - component to play video from one item data. First field contains title, second url to the video e.g. https://www.youtube.com/watch?v=v-PjgYDrg70
-* image - component to show one image from one item data. First field contains title, second url to the image.
+* video-player - component to play video from one item data. Field must contain url pointing to the video to be shown, e.g. https://www.youtube.com/watch?v=v-PjgYDrg70
+* image - component to show one image from one item data. Field must contain url pointing to the image to be shown, e.g. https://www.images.com/v-PjgYDrg70.jpeg
 """
 
 """ Load environment variable `NEXT_GEN_UI_AGENT_USE_ALL_COMPONENTS` to decide which set of allowed components to use for the LLM prompt.
@@ -64,17 +64,16 @@ async def component_selection_inference(
         # logger.debug(user_prompt)
         # logger.debug(input_data)
 
-    sys_msg_content = f"""You are helpful and advanced user interface design assistant. Based on the user query and JSON formatted data, select the best one UI component to visualize the data to the user.
-Generate response in the JSON format only. Select one component only.
+    sys_msg_content = f"""You are helpful and advanced user interface design assistant. Based on the "User query" and JSON formatted "Data", select the best UI component to visualize the "Data" to the user.
+Generate response in the JSON format only. Select one component only into "component".
 Provide the title for the component in "title".
 Provide reason for the component selection in the "reasonForTheComponentSelection".
 Provide your confidence for the component selection as a percentage in the "confidenceScore".
-Select only relevant data fields to be presented in the component. Do not bloat presentation. Show all the important info about the data item. Mainly include information the user asks for.
-Provide "name" for every field.
+Provide list of "fields" to be visualized in the UI component. Select only relevant data fields to be presented in the component. Do not bloat presentation. Show all the important info about the data item. Mainly include information the user asks for in User query.
+If the selected UI component requires specific fields mentioned in its description, provide them. Provide "name" for every field.
 For every field provide "data_path" containing path to get the value from the data. Do not use any formatting or calculations in the "data_path".
 
-Available UI components you can select from:
-{ui_components_description}
+Select one from there UI components: {ui_components_description}
 """
 
     sys_msg_content += """
@@ -114,7 +113,8 @@ Response example for one item data:
 {str(data)}
     """
 
-    logger.debug("LLM prompt: %s", prompt)
+    logger.debug("LLM system message:\n%s", sys_msg_content)
+    logger.debug("LLM prompt:\n%s", prompt)
 
     response = await inference.call_model(sys_msg_content, prompt)
     logger.debug("Component metadata LLM response: %s", response)
