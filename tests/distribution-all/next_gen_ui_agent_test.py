2b5c97e5997f7d586407d4264562bcf1d0e1fabf
Libor Krzyžanek <2361054+lkrzyzanek@users.noreply.github.com>
2025-07-22 13:00:32 +0200

    ci(NGUI-219): Publish 0.1.0 release pipeline (#135)

--- tests/distribution-all/next_gen_ui_agent_test.py
+++ tests/distribution-all/next_gen_ui_agent_test.py
@@ -30,18 +30,8 @@ def test_design_system_handler_rhds() -> None:
     assert "<rh-card" in rendition
 
 
-# marking as distribution because of rendering autodiscovery and the distribution package is need to be installed
-@pytest.mark.distribution
-def test_design_system_handler_patternfly() -> None:
-    agent = NextGenUIAgent()
-    res = agent.design_system_handler([test_component], "patternfly")
-    rendition = res[0].content
-    assert "<Card>" in rendition
-
-
 if __name__ == "__main__":
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
     test_design_system_handler_json()
     test_design_system_handler_rhds()
-    test_design_system_handler_patternfly()
