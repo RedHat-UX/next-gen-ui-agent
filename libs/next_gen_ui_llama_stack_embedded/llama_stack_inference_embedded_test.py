1849d8bcaf7f640aa12ec92ae744ede7d084ebe5
Libor Krzyžanek <2361054+lkrzyzanek@users.noreply.github.com>
2025-11-12 12:42:28 +0100

    chore(NGUI-429): pytest libraries bump, lock regenerated (#226)

--- libs/next_gen_ui_llama_stack_embedded/llama_stack_inference_embedded_test.py
+++ libs/next_gen_ui_llama_stack_embedded/llama_stack_inference_embedded_test.py
@@ -7,15 +7,16 @@ from next_gen_ui_llama_stack_embedded import init_inference_from_env
 
 
 def test_init_response_NOT_CONFIGURED() -> None:
-    os.environ.clear()
     inference = init_inference_from_env()
     assert inference is None
 
 
-def test_init_response_CONFIGURED_REMOTE_HOST() -> None:
-    os.environ.clear()
-    os.environ["INFERENCE_MODEL"] = "granite3.2:latest"
-    os.environ["LLAMA_STACK_HOST"] = "localhost"
+def test_init_response_CONFIGURED_REMOTE_HOST(monkeypatch) -> None:
+    # right way how to set env variables in pytest
+    # https://docs.pytest.org/en/latest/how-to/monkeypatch.html#monkeypatching-environment-variables
+    # calling os.environ.clear() BREAKS COVERAGE !!!
+    monkeypatch.setenv("INFERENCE_MODEL", "granite3.2:latest")
+    monkeypatch.setenv("LLAMA_STACK_HOST", "localhost")
     inference = init_inference_from_env()
     assert inference is not None
     assert isinstance(inference, LlamaStackAgentInference)
@@ -24,11 +25,10 @@ def test_init_response_CONFIGURED_REMOTE_HOST() -> None:
     assert inference.client.base_url == "http://localhost:5001"
 
 
-def test_init_response_CONFIGURED_REMOTE_HOST_PORT() -> None:
-    os.environ.clear()
-    os.environ["INFERENCE_MODEL"] = "granite3.2:latest"
-    os.environ["LLAMA_STACK_HOST"] = "localhost"
-    os.environ["LLAMA_STACK_PORT"] = "5002"
+def test_init_response_CONFIGURED_REMOTE_HOST_PORT(monkeypatch) -> None:
+    monkeypatch.setenv("INFERENCE_MODEL", "granite3.2:latest")
+    monkeypatch.setenv("LLAMA_STACK_HOST", "localhost")
+    monkeypatch.setenv("LLAMA_STACK_PORT", "5002")
     inference = init_inference_from_env()
     assert inference is not None
     assert isinstance(inference, LlamaStackAgentInference)
@@ -37,10 +37,9 @@ def test_init_response_CONFIGURED_REMOTE_HOST_PORT() -> None:
     assert inference.client.base_url == "http://localhost:5002"
 
 
-def test_init_response_CONFIGURED_REMOTE_URL() -> None:
-    os.environ.clear()
-    os.environ["INFERENCE_MODEL"] = "granite3.2:latest"
-    os.environ["LLAMA_STACK_URL"] = "https://localhost:5003"
+def test_init_response_CONFIGURED_REMOTE_URL(monkeypatch) -> None:
+    monkeypatch.setenv("INFERENCE_MODEL", "granite3.2:latest")
+    monkeypatch.setenv("LLAMA_STACK_URL", "https://localhost:5003")
     inference = init_inference_from_env()
     assert inference is not None
     assert isinstance(inference, LlamaStackAgentInference)
@@ -49,13 +48,12 @@ def test_init_response_CONFIGURED_REMOTE_URL() -> None:
     assert inference.client.base_url == "https://localhost:5003"
 
 
-def test_init_response_CONFIGURED_EMBEDDED() -> None:
-    os.environ.clear()
-    os.environ["INFERENCE_MODEL"] = "granite3.2:latest"
+def test_init_response_CONFIGURED_EMBEDDED(monkeypatch) -> None:
+    monkeypatch.setenv("INFERENCE_MODEL", "granite3.2:latest")
     # start with example ollama config and check expeced error is raised
     test_dir = os.path.dirname(os.path.abspath(__file__))
     config_file = os.path.join(test_dir, "examples", "llamastack-ollama.yaml")
-    os.environ["LLAMA_STACK_CONFIG_FILE"] = config_file
+    monkeypatch.setenv("LLAMA_STACK_CONFIG_FILE", config_file)
 
     try:
         init_inference_from_env()
