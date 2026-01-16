e48da07847b32f074d8de554618b3d5bdc1480b5
Hrithik-Gavankar <hgavanka@redhat.com>
2026-01-16 18:09:32 +0530

    chore: remove deprecated server files and modules for NGUI E2E tests

--- tests/ngui-e2e/server/agents/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Agent initialization and setup."""
-
-from .ngui import get_ngui_agent
-
-__all__ = ["get_ngui_agent"]
--- tests/ngui-e2e/server/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-"""API route handlers."""
-
-from .generate import router as generate_router
-from .health import router as health_router
-
-__all__ = ["health_router", "generate_router"]
