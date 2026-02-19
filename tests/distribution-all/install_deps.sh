6d80c1e4b7e85d08f744f4a7a318ac4524b2f121
Joachim Schuler <jdh.schuler@gmail.com>
2026-02-18 22:05:07 -0500

    deps

--- tests/distribution-all/install_deps.sh
+++ tests/distribution-all/install_deps.sh
@@ -3,4 +3,7 @@ pip uninstall -y ../../dist/next_gen_ui_*.whl
 
 echo INSTALLING all packages
 pip install pytest
+# LangChain/LangGraph (required by next_gen_ui_langgraph and distribution tests)
+pip install -r ../../libs/3rdparty/python/langchain-requirements.txt
+pip install -r ../../libs/3rdparty/python/langgraph-requirements.txt
 pip install ../../dist/next_gen_ui_*.whl -c ../../libs/3rdparty/python/llama-stack-client-constraints.txt
