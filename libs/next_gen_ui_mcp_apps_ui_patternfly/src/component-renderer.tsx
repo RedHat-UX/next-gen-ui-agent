c3a0c4c110e7cba89ba06572bd0411b9107dde43
Vlastimil Elias <velias@redhat.com>
2026-03-02 12:02:18 +0100

    ability to define action handler through NGUI component json

--- libs/next_gen_ui_mcp_apps_ui_patternfly/src/component-renderer.tsx
+++ libs/next_gen_ui_mcp_apps_ui_patternfly/src/component-renderer.tsx
@@ -36,20 +36,39 @@ interface ComponentRendererProps {
 export function ComponentRenderer({ app, configs, spacing = false }: ComponentRendererProps) {
 
   console.log("Initializing component renderer ...");
-  // TODO allow to add handlers more easily by consuments of this MCP Apps renderer, ideally through dedicated info in passed in NGUI `config`
+  // # TODO PoC code: allows to add action handlers by consuments of this MCP Apps renderer, through dedicated section in the NGUI `config` UI component json
   const registry = useComponentHandlerRegistry();
-  registry.registerItemClick("cve-list", (event, payload) => {
-    console.log("Item click handler – full payload:", payload);
-    if (payload.fields["cve"]?.value) {
-      app?.callServerTool({
-        name: "cve-detail",
-        arguments: {
-          cve_id: payload.fields["cve"]?.value,
-        },
-      });
+  for (const config of configs) {
+    // TODO add all the config structure validations ...
+    if (config.actions?.item_click) {
+      console.log("Registering item click handler for component type:", config.input_data_type);
+      const action = config.actions.item_click;
+      if (action.type === "tool_call") {
+        registry.registerItemClick(config.input_data_type, (event, payload) => {
+          console.log("Item click handler for component type ", config.input_data_type, " – full payload:", payload);
+          
+          const args: Record<string, any> = {};
+          if (action.arguments) {
+            for(const argumentKey of Object.keys(action.arguments)) {
+              args[argumentKey] = payload.fields[action.arguments[argumentKey]]?.value;
+            }
+          }
+          console.log("Calling tool:", action.tool, " with arguments:", args);
+          app?.callServerTool({
+            name: action.tool,
+            arguments: args,
+          });  
+        });
+      } else {
+        // TODO support other item click types like "message" to generate LLM message, "update-model-context" to update LLM context, or "open-link" to open an URL
+        console.error("Item click handler for component type:", config.input_data_type, " – unsupported action type:", action.type);
+      }
+    } else {
+      // TODO support other UI component user action types like "componet_actions", "item_actions", "item_field_clicks", "item_field_actions", see https://docs.google.com/document/d/1-3ZZfd-K2kK4CjerVn6SpqUa3CGWh6WBKrvOPM_EzOU
+      console.error("User action handler for component type:", config.input_data_type, " – unsupported user action");
     }
-  });
-
+  }
+  
   return (
     <div className="ngui-render-root">
       {configs.map((config, index) => (
